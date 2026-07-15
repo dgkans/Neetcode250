a28550b00a3e5a25a8aebb43b894a21ed4af5e71
Gaurav Desai <129505634+dgkans@users.noreply.github.com>
2026-07-14 21:20:22 -0700

    Time: 28 ms (99.01%), Space: 34.1 MB (83.17%) - LeetHub

---
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canTraverseAllPairs(self, nums: List[int]) -> bool:
+
+        if len(nums) == 1:                         # <-- some edge cases
+            return True
+        if 1 in nums: 
+            return False
+
+        nums = sorted(set(nums), reverse = True)    # <-- sort (big to little) and  
+        
+        if (n:=len(nums))==1 :                      #     deal with another edge case
+            return True          
+
+        for i in range(n-1):                        # <-- nums[i] >= nums[j]
+            for j in range(i+1,n):
+            
+                if gcd(nums[i],nums[j])-1:          # <-- i,j traversal exists; 
+                    nums[j]*= nums[i]               # <-- if an i,k traversal exists   
+                    break                           #     (for some index k), then now 
+                                                    #     a j,k traversal exists
+
+            else: return False                      # <-- no match means no traversal 
+
+        return True 
